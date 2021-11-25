b9864aa1efcd459ff038ef4a685bd48e3bc55042
carolcabralm <carolcabralm@msn.com>
2021-11-25 12:50:11 -0300

    atualizando bloco 5

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-JavaScript-eventos/exScript.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-JavaScript-eventos/exScript.js
@@ -72,3 +72,139 @@ function feriados(feriados) {
 }
 
 feriados("Feriados");
+
+//Exercício 3:
+//Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+//É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+function recebeClick() {
+  let clickB = document.getElementById('btn-holiday');
+  let holidays = document.querySelectorAll('.holiday');
+  let color = 'rgb(238,238,238)';
+  let newColor = 'pink';
+
+  clickB.addEventListener('click', function() {
+    for (let i = 0; i < holidays.length; i += 1) {
+      if (holidays[i].style.backgroundColor === newColor) {
+        holidays[i].style.backgroundColor = color;
+      }
+      else {
+        holidays[i].style.backgroundColor = newColor;
+      }
+    }
+  })    
+};
+recebeClick();
+
+
+//Exercício 4:
+//Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+//Adicione a este botão o ID "btn-friday" .
+//Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+
+
+function createBF (entrada) {
+  let buttonFriday = document.createElement('button');
+  buttonFriday.innerHTML = entrada;
+  buttonFriday.id = "btn-friday";
+  let divButCont = document.getElementsByClassName("buttons-container")[0];
+  divButCont.appendChild(buttonFriday);
+}
+createBF("Sexta-feira");
+
+//Exercício 5:
+//Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+//É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+
+function modText (fridays) {
+  let sextas = document.querySelectorAll('.friday');
+  let butFriday = document.getElementById('btn-friday');
+  let sentence = "Sextou!!!";
+
+  butFriday.addEventListener('click', function() {
+    for (let i = 0; i < sextas.length; i += 1) {
+      if (sextas[i].innerHTML !== sentence) {
+        sextas[i].innerHTML = sentence;
+      }
+      else {
+        sextas[i].innerHTML = fridays[i];
+      }
+    }
+  })
+}
+
+let dezFridays = [ 4, 11, 18, 25 ];
+modText(dezFridays);
+
+//Exercício 6:
+//Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+//Dica - Propriedade: event.target .
+
+function zoomIn() {
+  let day = document.querySelector ('#days');
+
+  day.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '40px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function zoomOut() {
+  let day = document.querySelector ('#days');
+
+  day.addEventListener('mouseout', function(event) {
+    event.target.style.fontSize = '20px';
+    event.target.style.fontWeight = '200';
+  })  
+};
+zoomIn();
+zoomOut();
+
+//Exercício 7:
+//Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+//O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function task(tarefa) {
+  let task = document.createElement('span');
+  let divTask = document.getElementsByClassName('my-tasks')[0];
+
+  task.innerHTML = tarefa;
+  divTask.appendChild(task);
+}
+task('estudar')
+
+
+//Exercício 8:
+//Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+//O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+//O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function legenda (cor) {
+  let divTask = document.getElementsByClassName('my-tasks')[0];
+  let element = document.createElement('div');
+  element.className = 'task';
+  element.style.backgroundColor = cor;
+  divTask.appendChild(element);
+}
+
+legenda('orange');
+
+
+//Exercício 9:
+//Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
+//Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+function select () {
+  let taskSelected = document.getElementsByClassName('task selected');
+  let task = document.querySelector('.task');
+
+  task.addEventListener('click', function(event) {
+    if(taskSelected.length === 0) {
+      event.target.className = 'task selected';
+    }
+    else {
+      event.target.className = 'task';
+    }
+  })
+}
+
+select();
